5e02f8d0d9f088ebf1233dbada06271d48d54224
GrungeElFz <elf0114_@hotmail.com>
2023-05-30 16:05:23 -0500

    Modified:   Updated `site` in `settings` (src/data/settings.ts)

--- src/data/settings.ts
+++ src/data/settings.ts
@@ -1,5 +1,5 @@
 export const settings = {
-  site: 'https://github.com/GrungeElFz/Astro_0x01000111_Landing.Page',
+  site: 'https://astro-0x01000111-landing-page.vercel.app/',
   name: '0x01000111',
   title: '0x01000111 | Landing Page',
   description: '0x01000111 | Landing Page',
