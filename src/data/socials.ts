618887ef0cbb4f3b0bd2444f8ea12dd8a2332c4b
GrungeElFz <elf0114_@hotmail.com>
2023-05-30 13:05:30 -0500

    Modified:   Updated `socials` links for footer (src/data/socials.ts)

--- src/data/socials.ts
+++ src/data/socials.ts
@@ -2,26 +2,16 @@ export const socials = [
   {
     title: 'Instagram',
     icon: 'mdi:instagram',
-    url: 'https://www.instagram.com/jaydanurwin/',
-  },
-  {
-    title: 'Twitter',
-    icon: 'mdi:twitter',
-    url: 'https://twitter.com/littlesticksdev',
-  },
-  {
-    title: 'YouTube',
-    icon: 'mdi:youtube',
-    url: 'https://youtube.com/c/LittleSticks',
+    url: 'https://www.instagram.com/0x01000111/',
   },
   {
     title: 'GitHub',
     icon: 'mdi:github',
-    url: 'https://github.com/littlesticksdev',
+    url: 'https://github.com/GrungeElFz/Astro_0x01000111_Landing.Page',
   },
   {
-    title: 'Dribbble',
-    icon: 'mdi:dribbble',
-    url: 'https://dribbble.com/littlesticksdev',  
+    title: 'Opensea',
+    icon: 'cryptocurrency:matic',
+    url: 'https://opensea.io/assets/matic/0x99b59ed222f9d228fb96ba54541731e8c6f1df64/1037/',  
   }
 ]
