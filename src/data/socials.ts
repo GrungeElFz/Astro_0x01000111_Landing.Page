5b1a7c6e29f4b204e14d5e836073eb06ab562d27
GrungeElFz <elf0114_@hotmail.com>
2023-05-30 22:00:23 -0500

    Modified:   Added `label` property to each item in `socials` array (src/data/socials.ts)

--- src/data/socials.ts
+++ src/data/socials.ts
@@ -3,15 +3,18 @@ export const socials = [
     title: 'Instagram',
     icon: 'mdi:instagram',
     url: 'https://www.instagram.com/0x01000111/',
+    label: 'Follow us on Instagram',
   },
   {
     title: 'GitHub',
     icon: 'mdi:github',
     url: 'https://github.com/GrungeElFz/Astro_0x01000111_Landing.Page',
+    label: 'Visit our GitHub repository',
   },
   {
     title: 'Opensea',
     icon: 'cryptocurrency:matic',
-    url: 'https://opensea.io/assets/matic/0x99b59ed222f9d228fb96ba54541731e8c6f1df64/1037/',  
-  }
-]
+    url: 'https://opensea.io/assets/matic/0x99b59ed222f9d228fb96ba54541731e8c6f1df64/1037/',
+    label: 'Explore our Opensea collection',
+  },
+];
