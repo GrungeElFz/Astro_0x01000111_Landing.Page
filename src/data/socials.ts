b969d492ae52d4dde678a3eff45a6e304862fe0e
GrungeElFz <elf0114_@hotmail.com>
2024-09-15 22:45:01 -0500

    Refactored:   Updated social labels from plural to singular (src/data/socials.ts)

--- src/data/socials.ts
+++ src/data/socials.ts
@@ -3,24 +3,24 @@ export const socials = [
     title: 'SoundCloud',
     icon: 'mdi:soundcloud',
     url: 'https://soundcloud.com/0x01000111',
-    label: 'Check out our mixes on SoundCloud',
+    label: 'Check out my mixes on SoundCloud',
   },
   {
     title: 'Instagram',
     icon: 'mdi:instagram',
     url: 'https://www.instagram.com/0x01000111/',
-    label: 'Follow us on Instagram',
+    label: 'Follow me on Instagram',
   },
   {
     title: 'GitHub',
     icon: 'mdi:github',
     url: 'https://github.com/GrungeElFz/Astro_0x01000111_Landing.Page',
-    label: 'Visit our GitHub repository',
+    label: 'Visit my GitHub repository',
   },
   {
     title: 'Opensea',
     icon: 'cryptocurrency:matic',
     url: 'https://opensea.io/assets/matic/0x99b59ed222f9d228fb96ba54541731e8c6f1df64/1037/',
-    label: 'Explore our Opensea collection',
+    label: 'Explore my Opensea collection',
   },
 ];
